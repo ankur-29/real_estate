1098051e194d8602206c4e373c455082e557bbbe
ankur-29 <ankur.anand.me044@gmail.com>
2024-07-23 14:07:27 +0530

    created routes and components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
-//import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import About from './components/About';
 
+const router = createBrowserRouter([
+  {
+    path: '/', element:<Homepage/>
+  },
+  {
+    path: '/register', element: <Signup/>
+  },
+  {
+    path: '/login', element: <Login/>
+  },
+  {
+    path: '/about', element: <About/>
+  },
+  {
+    path: '/profile', element: <Profile/>
+  }
+])
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='h-screen bg-green-200'>
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
